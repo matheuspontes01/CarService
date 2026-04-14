88790f133f4e242700dd0986bbc03c43f6647f30
matheuspontes01 <matheus.spontes0406@gmail.com>
2026-04-13 22:00:24 -0300

    Tela de Base e de Listagem de Veículos

--- sistema/urls.py
+++ sistema/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
+from django.conf.urls.static import static
 from sistema.views import Login
 
 urlpatterns = [
     path('', Login.as_view(), name='login'),
     path('admin/', admin.site.urls),
     path('veiculo/', include('veiculo.urls'), name='veiculo')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
